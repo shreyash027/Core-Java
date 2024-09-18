cb7e222da33e93e7f834427d360f1958407452bd
Shreyash Khamkar <136109608+shreyash027@users.noreply.github.com>
2024-09-18 06:27:22 -1200

    Add files via upload

---
+++ Conditional-05/$18_switch6.java
@@ -0,0 +1,18 @@
+class $18_switch6
+{
+	public static void main(String[] args)
+	{
+		int a = 100;
+
+		// switch(a)
+		// {
+		// 	case "hello" : System.out.println("hello");
+		// 					break;
+			
+		// 	case "hii" : System.out.println("hiii");
+		// 				 break;
+		// }
+
+		// the datatype of cas label and variable should be same
+	}
+}
