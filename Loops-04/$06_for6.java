3b3f5670d85942c4877724c1c1dbb527bbd0139b
Shreyash Khamkar <136109608+shreyash027@users.noreply.github.com>
2024-09-19 03:58:59 -1200

    Add files via upload

---
+++ Loops-04/$06_for6.java
@@ -0,0 +1,12 @@
+class $06_for6
+{
+	public static void main(String[] args)
+	{
+		// for(int i = 1; System.out.println("hello"); i++)
+		{
+			// System.out.println(i);
+		}
+	}
+}
+
+//  statements can be used within condition of for , while or do while loopcd ..
